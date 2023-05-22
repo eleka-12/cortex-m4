b5e0670593d8bd0095f6c0549eb8d1b580d3dd7c
Karthik-VG <karthikvg.engineer@gmail.com>
2023-05-22 23:16:08 +0530

    Adding the trails of and or not exor

--- tutorial 6 Bitwise operator in c/main.c
+++ tutorial 6 Bitwise operator in c/main.c
@@ -1,8 +1,8 @@
 #include "lm4f120h5qr.h"
 
 #define RED_LED (1U << 1)
-#define GREEN_LED (1U << 2)
-#define BLUE_LED (1U << 3)
+#define BLUE_LED (1U << 2)
+#define GREEN_LED (1U << 3)
 
 void delay(int val){      
     /* defined a function called delay and used while loop to count by increment method*/
@@ -18,27 +18,21 @@ void setupPORTF(){
 }
 
 void setLEDColor(int duration , int value){
-    GPIO_PORTF_DATA_R = value;
+    GPIO_PORTF_DATA_R |= value;
+    delay(duration);
+}
+
+void clearLEDColor(int duration , int value){
+    GPIO_PORTF_DATA_R &= ~value;
     delay(duration);
 }
 
 int main()
-{
-    unsigned int a = 0x5A5A5A5A;
-    unsigned int b = 0xDEADBEEF;
-    unsigned int c;
-
-    c = a|b;
-    c = a&b;
-    c = a^b;
-    c = ~b;
-    c = b<<1;
-    c = b>>3;
-    int d = c;
-    
+{    
+  setLEDColor(1000000,GREEN_LED);
   setupPORTF(); //setting up PORT F register to RGB LED
   while(1){
-    setLEDColor(4000000 , 0x04); // setting BLUE LED on by setting data to 0x0100
-    setLEDColor(1000000 , 0x00); 
+    setLEDColor(1000000 , GREEN_LED); // setting BLUE LED on by setting data to 0x0100
+    clearLEDColor(1000000 , GREEN_LED); 
   }
 }
